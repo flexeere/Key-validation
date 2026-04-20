b832a56880eda61355f46381fbad6c31de0fa3b1
Shrinjay Shresth <shrinjayshresth1@gmail.com>
2026-04-20 21:13:45 +0530

    fix: change register route to root URL

---
+++ pull_start_down.py
@@ -0,0 +1,24 @@
+import paramiko
+import sys
+
+host = '103.124.208.21'
+user = 'advps'
+password = '93jVJS7l1k6g'
+
+try:
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(host, username=user, password=password, timeout=10)
+    
+    cmd = "cd ~/Key-validation && git pull && echo '93jVJS7l1k6g' | sudo -S docker-compose down && echo '93jVJS7l1k6g' | sudo -S docker-compose up -d --build"
+    stdin, stdout, stderr = ssh.exec_command(cmd)
+    
+    out = stdout.read().decode('utf-8', errors='replace')
+    err = stderr.read().decode('utf-8', errors='replace')
+    if out: print("STDOUT:\n", out)
+    if err: print("STDERR:\n", err)
+    
+except Exception as e:
+    pass
+finally:
+    ssh.close()
--- registration/urls.py
+++ registration/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.RegisterCompanyView.as_view(), name='register_company'),
+    path('', views.RegisterCompanyView.as_view(), name='register_company'),
     path('verify/<str:token>/', views.VerifyKeyView.as_view(), name='verify_email'),
 ]
